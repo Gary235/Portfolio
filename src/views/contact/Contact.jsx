11e4dedfb06fdfab0228d027e9e51e51f8f10080
gary.berkman <gary.berkman@wearepsh.com>
2022-05-19 09:07:53 -0300

    finish form contact

--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -1,18 +1,36 @@
-import { ContactContainer, Form, InputWrapper, TextInput } from "./styles"
+import { useState } from "react"
+import { ContactContainer, Form, InputWrapper, Submit, TextArea, TextInput } from "./styles"
 
 const Contact = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const inputsSetters = { 'name': setName, 'email': setEmail, 'message': setMessage }
+
+  const handleInputChange = event => {
+    const {target: { name, value }} = event;
+    const setter = inputsSetters[name];
+
+    setter(value)
+  }
 
   return (
     <ContactContainer id="contact">
       <Form>
-        <InputWrapper label="'name'">
-          <TextInput type="text" name="name" label="name" />
+        <InputWrapper label="'name'" showInsideInput={!name}>
+          <TextInput tabIndex={1} value={name} type="text" name="name" label="name" onChange={handleInputChange} required />
         </InputWrapper>
 
-        <InputWrapper label="'email'">
-          <TextInput type="email" name="email" />
+        <InputWrapper label="'email'" showInsideInput={!email}>
+          <TextInput tabIndex={2} value={email} type="email" name="email" onChange={handleInputChange} required />
         </InputWrapper>
 
+        <InputWrapper label="'message'" showInsideInput={!message}>
+          <TextArea tabIndex={3} value={message} name="message" onChange={handleInputChange} required />
+        </InputWrapper>
+
+        <Submit tabIndex={4} type="submit" value="send" />
       </Form>
     </ContactContainer>
   )
--- src/views/contact/styles.js
+++ src/views/contact/styles.js
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 export const ContactContainer = styled.section`
   background-color: #d11d95;
-  height: 15vh;
+  height: fit-content;
+  padding: 2rem 0;
   width: 100%;
   border-radius: 5px;
   background-size: 120px 120px;
@@ -22,64 +23,99 @@ export const ContactContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin-bottom: 5rem;
 `;
 
 export const Form = styled.form`
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  display: grid;
+  padding: 0 10%;
+
+  grid-template-areas: 'name message' 'email message' 'submit submit';
+  gap: 1rem;
 `;
-
-
-// export const TextInput = styled.input`
-//   width: 100%;
-//   height: 2.5rem;
-//   padding: 0 0.75rem;
-//   background-color: #f8f8f8;
-//   border-radius: 0.5rem;
-//   border: none;
-//   /* outline: none; */
-
-//   &:focus {
-
-//   }
-// `;
 
 export const InputWrapper = styled.div`
   position: relative;
-  width: 80%;
-  max-width: 25rem;
-  height: 2.5rem;
+  width: 100%;
+  
+  height: ${({label}) => label === "'message'" ? '7.8rem' : '3.4rem'};
+  grid-area: ${({label}) => label.slice(1, label.length - 1)};
+
+  display: flex;
+  align-items: center;
 
   &::after {
     content: ${({label}) => label || ''};
     position: absolute;
     left: 0.75rem;
-    top: 25%;
     z-index: 20;
-    color: black;
-    font-size: 12px;
+    color: #8b8b8b;
     
-    /* ${TextInput}:active & {
-      content: ''
-    } */
+    transition: top 0.2s;
+    
+    font-size: ${({showInsideInput}) => showInsideInput ? '14px' : '11px'};
+    top: ${({showInsideInput}) => showInsideInput ? '1rem' : '2px'};
   }
-
 `;
 
 export const TextInput = styled.input`
   width: 100%;
-  height: 2.5rem;
+  height: 3.4rem;
   padding: 0 0.75rem;
   background-color: #f8f8f8;
-  border-radius: 0.5rem;
+  border-radius: 0.3rem;
   border: none;
-  outline: none;
+  outline: 3px solid transparent;
 
-  /* &:focus {
-    ${InputWrapper} {
-      display: none;
-    }
-  } */
+  transition: outline 0.2s;
+
+  &:focus {
+    outline: 3px solid #ffffff50;
+  }
 `;
+
+export const TextArea = styled.textarea`
+  min-width: 100%;
+  min-height: 7.8rem;
+  padding: 1rem 0.75rem 0;
+  background-color: #f8f8f8;
+  border-radius: 0.35rem;
+  border: none;
+  outline: 3px solid transparent;
+
+  resize: none;
+  overflow: hidden;
+
+  transition: outline 0.2s;
+
+  &:focus {
+    outline: 3px solid #ffffff50;
+  }
+`
+
+export const Submit = styled.input`
+  grid-area: submit;
+  
+  margin-top: 0.5rem;
+  width: fit-content;
+  padding: 1rem 2rem;
+  justify-self: center;
+  cursor: pointer;
+  border-radius: 0.2rem;
+  background: #000;
+  color: #fff;
+  font-weight: 600;
+
+  border: none;
+  outline: 3px solid transparent;
+
+  transition: background 0.2s, outline 0.2s;
+
+  &:hover {
+    background: #2b2b2b;
+  }
+  &:focus {
+    outline: 3px solid #00000050;
+  }
+`
